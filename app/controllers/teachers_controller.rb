5b1ef728304543fe281694946b33e2fbcd1840cf
Saulo Pordeus <saulospordues@gmail.com>
2021-05-17 09:46:13 -0300

    Crud Finalizado não testado

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,15 +1,37 @@
 class TeachersController < ApplicationController
     def index
-    @teachers = Teacher.all
+      @teachers = Teacher.all
+    end
+
+    def show
+      @teacher = Teacher.find(params[:id])
     end
 
     def new
-        @teacher = Teacher.new
+      @teacher = Teacher.new
     end
 
     def create
-        @teacher = Teacher.create!(params.require(:teacher).permit(:name, :email, :bio, :avatar))
-        redirect_to teachers_path
+      @teacher = Teacher.create!(params.require(:teacher).permit(:name, :email, :bio, :avatar))
+      session[:teacher] = teacher.id
+      redirect_to teachers_path
     end
+
+    def edit
+      @teacher = Teacher.find(params[:id])
+    end
+    
+    def update
+      @teacher = Teacher.find(params[:id])
+      @teacher.update(params.require(:teacher).permit(:name, :email, :bio, :avatar))
+      redirect_to @teacher
+    end
+
+    def destroy
+      teacher = Teacher.find(params[:id])
+      teacher.destroy
+      redirect_to teachers_path
+    end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
   resources :courses, only: [:index, :show]
-  resources :teachers, only: [:index, :new, :create]
+  resources :teachers, only: [:index, :new, :create, :show, :edit, :update, :delete, :destroy]
 end
